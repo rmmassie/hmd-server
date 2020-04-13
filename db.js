d2e30b49837ae6dad8074ce1c88a7ff29f6ef16a
Matt Massie <rmmassie@gmail.com>
2020-04-13 19:11:15 -0400

    Heroku TS Fixc

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL || `postgresql://postgres:${encodeURIComponent(process.env.DBPW)}@localhost/helpMeDecide`, 
+    {
     dialect: 'postgres',
     logging: false //remove for verbose logging in console
-});
+    });
 //console.log(process.env.DBPW)
 sequelize.authenticate().then(
     function() {
